cd39710027c6d9d2e9e8de465df6a92989f5d41d
Carlos <carloscastrogames@gmail.com>
2021-11-09 09:56:15 -0300

    🏷️ Entities: Add Product

---
+++ lib/domain/entities/product.dart
@@ -0,0 +1,12 @@
+class Product {
+  final String name;
+  final String description;
+  final String image;
+  final String price;
+  Product({
+    required this.name,
+    required this.description,
+    required this.image,
+    required this.price,
+  });
+}
